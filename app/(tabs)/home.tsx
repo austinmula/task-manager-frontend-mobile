36a37de2017c3b49d862c8ebd234b0fa8803463c
austinmula <maustin64.ma@gmail.com>
2025-07-11 17:32:39 +0300

    Add authentication and tab navigation layouts; remove unused components and files

---
+++ app/(tabs)/home.tsx
@@ -0,0 +1,15 @@
+import { Text, View } from "react-native";
+
+export default function HomeScreen() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text>Edit app/(tabs)/home.tsx to edit this screen.</Text>
+    </View>
+  );
+}
